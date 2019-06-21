1e4a2573f01b3e14de53da4888f7add576729f46
trevorb <trevor.barnes91@gmail.com>
2019-06-20 22:42:48 -0700

    forms and user accounts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView # new
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # new
+from django.urls import reverse_lazy # new
 from .models import Post
+
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -8,6 +11,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # new
     model = Post
     template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
 
 
 # Create your views here.
--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig', # new
+    'accounts.apps.AccountsConfig', # new
 
 ]
 
@@ -122,3 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home' # new
+
